34f8058eaa2204731fd977fb07a2bdaf9ca08f08
Francisco Jose Moguel <135240697+fjmoguel@users.noreply.github.com>
2024-09-11 14:42:39 -0400

    Uploading assignment 0
    Hi 506

---
+++ assignment-0.py
@@ -0,0 +1,7 @@
+def kinder_sum():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(f"The sum of {num1} and {num2} is {num1 + num2}")
+
+if __name__ == "__main__":
+    kinder_sum()
